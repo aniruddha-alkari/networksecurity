a7cb12c47eee90ac660e9ab5639cc41c814e72da
aniruddha-alkari <aniruddha.alkari@gmail.com>
2026-05-21 10:18:07 +0530

    MongoDB password hide

--- test_mongodb_connection.py
+++ test_mongodb_connection.py
@@ -2,7 +2,7 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://aniruddhaalkari_db_user:Admin123@cluster0.xzjozpd.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://aniruddhaalkari_db_user:<password>@cluster0.xzjozpd.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
