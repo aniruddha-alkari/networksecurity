eaad13932d55df89fa2a9d0ae43442c9fbdfa2ad
aniruddha-alkari <aniruddha.alkari@gmail.com>
2026-05-22 13:53:46 +0530

    Data Validation Completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -14,13 +14,27 @@ FILE_NAME:str = "phisingData.csv"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
 '''
 Data Ingestion related constant start with Data_Ingestion VAR NAME
 '''
-
 DATA_INGESTION_COLLECTION_NAME = "NetworkData"
 DATA_INGESTION_DATABASE_NAME = "ANIRUDDHA_AI"
 DATA_INGESTION_DIR_NAME = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR = "feature_store"
 DATA_INGESTION_INGESTED_DIR = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+'''
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+'''
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
